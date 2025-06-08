9ce25157c2587ec72fdd63ec5e70072f76ff85d6
Deniz Terzioglu <deniz.terzioglu@outlook.com>
2025-06-09 00:02:53 +0100

    feat: add setting to enable custom Python code block and update notebook editor command description

--- src/components/Settings.ts
+++ src/components/Settings.ts
@@ -1,57 +1,41 @@
-import { App, PluginSettingTab, Setting } from "obsidian";
-import { CodeExecutor } from "./CodeExecutor";
-import { KernelManager } from "./KernelManager";
+import {App, PluginSettingTab, Setting} from "obsidian";
+import {CodeExecutor} from "./CodeExecutor";
 import JupyMDPlugin from "../main";
 
 export class JupyMDSettingTab extends PluginSettingTab {
 	plugin: JupyMDPlugin;
 	executor: CodeExecutor;
-	kernelManager: KernelManager;
 
 	constructor(app: App, plugin: JupyMDPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
-		this.kernelManager = new KernelManager(this.plugin, this.app);
 		this.executor = new CodeExecutor(this.plugin, this.app);
 	}
 
 	display(): void {
-		const { containerEl } = this;
+		const {containerEl} = this;
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName("Default Python kernel")
-			.setDesc("Select the default Python kernel for execution")
-			.addDropdown((dropdown) => {
-				this.plugin.settings.availableKernels.forEach((kernel) => {
-					dropdown.addOption(kernel, kernel);
-				});
-				dropdown.setValue(this.plugin.settings.defaultKernel);
-				dropdown.onChange(async (value) => {
-					this.plugin.settings.defaultKernel = value;
-					await this.plugin.saveSettings();
-				});
-			});
-
-		new Setting(containerEl)
-			.setName("Detect kernels")
-			.setDesc("Refresh list of available kernels")
-			.addButton((button) => {
-				button.setButtonText("Detect").onClick(async () => {
-					await this.kernelManager.detectAvailableKernels();
-					this.display();
-				});
-			});
-
-		new Setting(containerEl)
 			.setName("Jupyter notebook editor launch command")
-			.setDesc("Specify the command to launch Jupyter notebooks in your preferred editor (e.g., 'code' for VS Code, 'jupyter-lab' for Jupyter Lab)")
+			.setDesc("Specify the command to launch Jupyter notebooks in your preferred editor (e.g., 'code' for VS Code, 'jupyter-lab' for Jupyter Lab, 'pycharm64.exe' for PyCharm, etc.)")
 			.addText((text) => {
 				text.setValue(this.plugin.settings.notebookEditorCommand)
 					.onChange(async (value) => {
 						this.plugin.settings.notebookEditorCommand = value;
 						await this.plugin.saveSettings();
 					});
+			})
+
+		new Setting(containerEl)
+			.setName("Enable custom Python code block")
+			.setDesc("If disabled, the default Obsidian code block will be used. Requires restart to take effect.")
+			.addToggle((toggle) => {
+				toggle.setValue(this.plugin.settings.enableCodeBlocks)
+				toggle.onChange(async (value) => {
+					this.plugin.settings.enableCodeBlocks = value;
+					await this.plugin.saveSettings();
+				})
 			})
 	}
 }
