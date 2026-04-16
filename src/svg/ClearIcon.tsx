ff42a1dca3393e6ef42dc8a7cdd66336892cda3c
Deniz Terzioglu <deniz.terzioglu@outlook.com>
2026-04-16 10:03:52 +0100

    feat: reduce stroke width of clear icon

--- src/svg/ClearIcon.tsx
+++ src/svg/ClearIcon.tsx
@@ -6,7 +6,7 @@ export const ClearIcon = ({className = ""}) => (
 		viewBox="0 0 24 24"
 		fill="none"
 		stroke="currentColor"
-		strokeWidth="2"
+		strokeWidth="1.7"
 		strokeLinecap="round"
 		strokeLinejoin="round"
 	>
